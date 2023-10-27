f9c582dae641c01b09f520f2c3f7f7af33c4d56d
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2023-10-27 15:57:02 +0200

    updated callback-based producer

--- calico/devices/observable_videocapture.cpp
+++ calico/devices/observable_videocapture.cpp
@@ -4,3 +4,8 @@ calico::devices::observable_videocapture::observable_videocapture()
 	: m_capture(0, cv::CAP_DSHOW)
 {
 }
+
+void calico::devices::observable_videocapture::stop()
+{
+	m_worker.request_stop();
+}
--- calico/producers/image_producer_callback.h
+++ calico/producers/image_producer_callback.h
@@ -1,20 +1,20 @@
 #pragma once
-#include <stop_token>
+
 #include <so_5/all.hpp>
 #include "../devices/observable_videocapture.h"
 
 namespace calico::producers
 {
 	// grabs frames from the default camera (e.g. webcam) and sends them to the specified message box (callback-based device)
+	// reacts to 'start_acquisition_command' and 'stop_acquisition_command' to control the acquisition
 	class image_producer_callback final : public so_5::agent_t
 	{
 	public:
-		image_producer_callback(so_5::agent_context_t ctx, so_5::mbox_t channel);
-		void so_evt_start() override;
-		void so_evt_finish() override;
+		image_producer_callback(so_5::agent_context_t ctx, so_5::mbox_t channel, so_5::mbox_t commands);
+		void so_define_agent() override;
 	private:
 		so_5::mbox_t m_channel;
+		so_5::mbox_t m_commands;
 		devices::observable_videocapture m_device;
-		std::stop_source m_stop_source;
 	};
 }
