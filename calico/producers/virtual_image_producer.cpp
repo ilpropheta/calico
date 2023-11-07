9dacdfcea616ffea3350c976d5b11eb89fce96ce
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2023-11-07 15:42:32 +0100

    updated virtual_image_producer

--- calico/producers/virtual_image_producer.cpp
+++ calico/producers/virtual_image_producer.cpp
@@ -1,21 +1,39 @@
 #include "virtual_image_producer.h"
 #include <opencv2/imgcodecs/imgcodecs.hpp>
+#include "../signals.h"
 
 using namespace std::chrono_literals;
 
-calico::producers::virtual_image_producer::virtual_image_producer_broker::virtual_image_producer_broker(so_5::agent_context_t c, std::stop_source stop_source)
-	: agent_t(std::move(c)), m_stop_source(std::move(stop_source))
+calico::producers::virtual_image_producer::virtual_image_producer(so_5::agent_context_t ctx, so_5::mbox_t channel, so_5::mbox_t commands, std::filesystem::path path)
+	: agent_t(std::move(ctx)), m_channel(std::move(channel)), m_commands(std::move(commands)), m_path(std::move(path))
 {
 }
 
-void calico::producers::virtual_image_producer::virtual_image_producer_broker::so_evt_finish()
+void calico::producers::virtual_image_producer::so_define_agent()
 {
-	m_stop_source.request_stop();
-}
+	st_started.event([this](so_5::mhood_t<grab_image>) {
+		const auto tic = std::chrono::steady_clock::now();
+		cv::Mat mat;
+		m_current_it = std::find_if(m_current_it, {}, [](const auto& e) {
+			return e.is_regular_file();
+		});
+		so_5::send<cv::Mat>(m_channel, cv::imread(m_current_it->path().string()));
+		const auto elapsed = std::min(20ms, std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - tic));
+		so_5::send_delayed<grab_image>(*this, 20ms - elapsed);
+		if (++m_current_it == std::filesystem::directory_iterator{})
+		{
+			m_current_it = std::filesystem::directory_iterator{ m_path };
+		}
+	}).event(m_commands, [this](so_5::mhood_t<stop_acquisition_command>) {
+		st_stopped.activate();
+	});
 
-calico::producers::virtual_image_producer::virtual_image_producer(so_5::agent_context_t ctx, so_5::mbox_t channel, std::filesystem::path path)
-	: agent_t(std::move(ctx)), m_channel(std::move(channel)), m_path(std::move(path))
-{
+	st_stopped.event(m_commands, [this](so_5::mhood_t<start_acquisition_command>) {
+		st_started.activate();
+		so_5::send<grab_image>(*this);
+	});
+
+	st_stopped.activate();
 }
 
 void calico::producers::virtual_image_producer::so_evt_start()
@@ -25,22 +43,5 @@ void calico::producers::virtual_image_producer::so_evt_start()
 		throw std::runtime_error("Can't open virtual device directory");
 	}
 
-	introduce_child_coop(*this, so_5::disp::active_obj::make_dispatcher(so_environment()).binder(), [this](so_5::coop_t& c) {
-		c.make_agent<virtual_image_producer_broker>(m_stop_source);
-	});
-
-	auto it = std::filesystem::directory_iterator{ m_path };
-	const auto st = m_stop_source.get_token();
-	while (!st.stop_requested())
-	{
-		if (it->is_regular_file())
-		{
-			so_5::send<cv::Mat>(m_channel, cv::imread(it->path().string()));
-			std::this_thread::sleep_for(20ms); // we assume 50 FPS
-		}
-		if (++it == std::filesystem::directory_iterator{})
-		{
-			it = std::filesystem::directory_iterator{ m_path };
-		}
-	}
+	m_current_it = std::filesystem::directory_iterator{ m_path };
 }
