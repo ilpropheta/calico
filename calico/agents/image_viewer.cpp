7d9a49f21c2fed0ff6f54c644976ad55e7d3a8da
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2023-11-10 08:55:19 +0100

    image viewer now closes the window when the stream goes idle

--- calico/agents/image_viewer.cpp
+++ calico/agents/image_viewer.cpp
@@ -1,6 +1,8 @@
 #include "image_viewer.h"
 #include <opencv2/highgui/highgui.hpp>
 
+using namespace std::chrono_literals;
+
 calico::agents::image_viewer::image_viewer(so_5::agent_context_t ctx, so_5::mbox_t channel)
 	: agent_t(std::move(ctx)), m_channel(std::move(channel))
 {
@@ -8,8 +10,16 @@ calico::agents::image_viewer::image_viewer(so_5::agent_context_t ctx, so_5::mbox
 
 void calico::agents::image_viewer::so_define_agent()
 {
-	so_subscribe(m_channel).event([](const cv::Mat& image) {
-		imshow("Image Viewer", image);
-		cv::waitKey(25);
-	});
+	st_handling_images
+		.event(m_channel, [this](const cv::Mat& image) {
+			imshow(m_title, image);
+			cv::waitKey(25);
+			st_handling_images.time_limit(500ms, st_stream_down);
+		})
+		.on_exit([this] { cv::destroyWindow(m_title); });
+
+	st_stream_down
+		.transfer_to_state<cv::Mat>(m_channel, st_handling_images);
+
+	st_stream_down.activate();
 }
