1e86af7e9b704090a1011e9215e2aa483e9b8d40
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2023-11-28 11:38:34 +0100

    sending errors from image_producer_recursive

--- calico/producers/image_producer_recursive.cpp
+++ calico/producers/image_producer_recursive.cpp
@@ -1,5 +1,6 @@
 #include "image_producer_recursive.h"
 #include "../signals.h"
+#include "../errors.h"
 
 calico::producers::image_producer_recursive::image_producer_recursive(so_5::agent_context_t ctx, so_5::mbox_t channel, so_5::mbox_t commands)
 	: agent_t(std::move(ctx)), m_channel(std::move(channel)), m_commands(std::move(commands)), m_capture(0, cv::CAP_DSHOW)
@@ -9,9 +10,15 @@ calico::producers::image_producer_recursive::image_producer_recursive(so_5::agen
 void calico::producers::image_producer_recursive::so_define_agent()
 {
 	st_started.event([this](so_5::mhood_t<grab_image>) {
-		cv::Mat mat;
-		m_capture >> mat;
-		so_5::send<cv::Mat>(m_channel, std::move(mat));
+		cv::Mat image;
+		if (m_capture.read(image))
+		{
+			so_5::send<cv::Mat>(m_channel, std::move(image));
+		}
+		else
+		{
+			so_5::send<device_error>(m_channel, "read error", device_error_type::read_error);
+		}
 		so_5::send<grab_image>(*this);
 	}).event(m_commands, [this](so_5::mhood_t<stop_acquisition_command>) {
 		st_stopped.activate();
