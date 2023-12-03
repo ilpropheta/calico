88ecfc6664d8a62d0f83964b39f10f68047476cf
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2023-12-03 19:26:02 +0100

    introduced image_saver and hided image_save_worker as its implementation detail

--- calico/agents/image_saver.h
+++ calico/agents/image_saver.h
@@ -5,16 +5,15 @@
 namespace calico::agents
 {
 	// saves the input images to a specified folder
-	class image_save_worker final : public so_5::agent_t
+	class image_saver final : public so_5::agent_t
 	{
 	public:
-		image_save_worker(so_5::agent_context_t ctx, so_5::mchain_t input, std::filesystem::path root_folder);
+		image_saver(so_5::agent_context_t ctx, so_5::mbox_t input, std::filesystem::path root_folder);
 		void so_evt_start() override;
+		void so_evt_finish() override;
 	private:
-		so_5::mchain_t m_input;
+		so_5::mbox_t m_input;
+		so_5::mchain_t m_chain;
 		std::filesystem::path m_root_folder;
-		int m_counter = 0;
-		static inline int global_id = 0;
-		int m_worker_id = global_id++;
 	};
 }
--- calico/calico.cpp
+++ calico/calico.cpp
@@ -12,20 +12,13 @@ int main()
 	const so_5::wrapped_env_t sobjectizer;
 	const auto main_channel = sobjectizer.environment().create_mbox("main");
 	auto commands_channel = sobjectizer.environment().create_mbox("commands");
-	auto save_buffer = create_mchain(sobjectizer.environment());
-	const auto chain_guard = auto_close_retain_content(save_buffer);
 
 	sobjectizer.environment().introduce_coop(so_5::disp::active_obj::make_dispatcher(sobjectizer.environment()).binder(), [&](so_5::coop_t& c) {
 		c.make_agent<calico::producers::image_producer_recursive>(main_channel, commands_channel);
 		c.make_agent<calico::agents::remote_control>(commands_channel);
 		c.make_agent<calico::agents::image_viewer_live>(main_channel);
 		const auto resized = c.make_agent<calico::agents::image_resizer>(main_channel, 3.0)->output();
-
-		const auto binding = c.take_under_control(std::make_unique<so_5::single_sink_binding_t>());
-		binding->bind<cv::Mat>(resized, wrap_to_msink(save_buffer->as_mbox()));
-
-		c.make_agent<calico::agents::image_save_worker>(save_buffer, "c:/images");
-		c.make_agent<calico::agents::image_save_worker>(save_buffer, "c:/images");
+		c.make_agent<calico::agents::image_saver>(resized, "c:/images");
 	});
 
 	calico::utils::wait_for_stop(ctrl_c);
