5be1730a522578574685a407e436edb192e58c9d
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2023-12-18 10:25:40 +0100

    added test on virtual producer that failes at the startup

--- calico_tests/virtual_image_producer_tests.cpp
+++ calico_tests/virtual_image_producer_tests.cpp
@@ -36,3 +36,23 @@ TEST(virtual_image_producer_tests, should_send_images_from_folder_after_receivin
 	EXPECT_THAT(sum(actual[0] != actual[3]), testing::Eq(cv::Scalar(0, 0, 0, 0)));
 	EXPECT_THAT(sum(actual[1] != actual[4]), testing::Eq(cv::Scalar(0, 0, 0, 0)));
 }
+
+TEST(virtual_image_producer_tests, when_folder_is_nonexistent_should_throw_exception_after_startup)
+{
+	const so_5::wrapped_env_t sobjectizer;
+
+	std::atomic cooperation_deregistered_reason = 0;
+	
+	sobjectizer.environment().introduce_coop([&](so_5::coop_t& c) {
+		c.set_exception_reaction(so_5::deregister_coop_on_exception);
+		c.make_agent<calico::producers::virtual_image_producer>(sobjectizer.environment().create_mbox(), sobjectizer.environment().create_mbox(), R"(C:/geppo)");
+
+		c.add_dereg_notificator([&](so_5::environment_t&, const so_5::coop_handle_t&, const so_5::coop_dereg_reason_t& why) noexcept {
+			cooperation_deregistered_reason = why.reason();
+			cooperation_deregistered_reason.notify_one();
+			});
+		});
+
+	cooperation_deregistered_reason.wait(cooperation_deregistered_reason.load());
+	EXPECT_THAT(cooperation_deregistered_reason, testing::Eq(so_5::dereg_reason::unhandled_exception));
+}
