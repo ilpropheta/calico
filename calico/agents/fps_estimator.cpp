c329dfc70acfc17ff38798a6ae17a93c198d902b
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2024-02-21 10:12:54 +0100

    changed fps_estimator to log only when stream is active

--- calico/agents/fps_estimator.cpp
+++ calico/agents/fps_estimator.cpp
@@ -1,39 +1,47 @@
 #include "fps_estimator.h"
 #include <syncstream>
 #include <format>
+#include <ranges>
 #include <opencv2/core/mat.hpp>
 
 using namespace std::chrono_literals;
 
 calico::agents::fps_estimator::fps_estimator(so_5::agent_context_t ctx, std::vector<so_5::mbox_t> inputs)
-	: agent_t(std::move(ctx)), m_inputs(std::move(inputs))
+	: agent_t(std::move(ctx)), m_inputs(move(inputs))
 {
-}
-
-void calico::agents::fps_estimator::so_evt_start()
-{
-	m_timer = so_5::send_periodic<measure_fps>(so_direct_mbox(), 5s, 5s);
-	m_start = std::chrono::steady_clock::now();
 }
 
 void calico::agents::fps_estimator::so_define_agent()
 {
 	for (const auto& input : m_inputs)
 	{
-		m_counters[input->query_name()] = 0;
-		so_subscribe(input).event([channel_name = input->query_name(), this](const cv::Mat&) {
+		st_handling_images.event(input, [channel_name = input->query_name(), this](const cv::Mat&) {
 			++m_counters[channel_name];
+			st_handling_images.time_limit(500ms, st_stream_down);
 		});
+
+		st_stream_down
+			.transfer_to_state<cv::Mat>(input, st_handling_images);
 	}
 
-	so_subscribe_self().event([this](so_5::mhood_t<measure_fps>) {
-		const auto elapsed_time = std::chrono::duration<double>(std::chrono::steady_clock::now() - m_start);
-		for (auto& [id, counter] : m_counters)
-		{
-			const auto fps = counter / elapsed_time.count();
-			std::osyncstream(std::cout) << std::format("Estimated fps @{}=~{:.2f} ({} frames in ~{})\n", id, fps, counter, std::chrono::round<std::chrono::seconds>(elapsed_time));
-			counter = 0;
-		}
-		m_start = std::chrono::steady_clock::now();
+	st_handling_images.on_enter([this] {
+      for (auto& counter : m_counters | std::views::values)
+          counter = 0;
+      m_timer = send_periodic<measure_fps>(so_direct_mbox(), 5s, 5s);
+      m_start = std::chrono::steady_clock::now();
+	})
+	.on_exit([this] {
+      m_timer.release();
+	}).event([this](mhood_t<measure_fps>) {
+      const auto elapsed_time = std::chrono::duration<double>(std::chrono::steady_clock::now() - m_start);
+      m_start = std::chrono::steady_clock::now();
+      for (auto& [id, counter] : m_counters)
+      {
+          const auto fps = counter / elapsed_time.count();
+          std::osyncstream(std::cout) << format("Estimated fps @{}=~{:.2f} ({} frames in ~{})\n", id, fps, counter, std::chrono::round<std::chrono::seconds>(elapsed_time));
+          counter = 0;
+      }
 	});
+
+	st_stream_down.activate();
 }
--- calico/agents/fps_estimator.h
+++ calico/agents/fps_estimator.h
@@ -3,15 +3,17 @@
 
 namespace calico::agents
 {
-	// estimates and logs the number of frames that are transmitted on a several channels, per second
+	// estimates and logs the number of frames that are transmitted on several channels, per second.
+	// Logs are displayed only when images are flowing on at least one channel.
 	class fps_estimator final : public so_5::agent_t
 	{
 		struct measure_fps final : so_5::signal_t {};
 	public:
 		fps_estimator(so_5::agent_context_t ctx, std::vector<so_5::mbox_t> inputs);
-		void so_evt_start() override;
 		void so_define_agent() override;
 	private:
+		state_t st_handling_images{ this };
+		state_t st_stream_down{ this };
 		std::vector<so_5::mbox_t> m_inputs;
 		so_5::timer_id_t m_timer;
 		std::map<std::string, unsigned> m_counters;
