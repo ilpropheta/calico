abddc8000169fdae3808f4acf68d50492c3a895d
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2024-02-28 09:30:53 +0100

    added service_time_estimator_dispatcher + performance test

--- calico_tests/performance_tests.cpp
+++ calico_tests/performance_tests.cpp
@@ -3,6 +3,7 @@
 #include <future>
 #include <opencv2/imgcodecs.hpp>
 #include "frequency_calculator.h"
+#include "service_time_estimator_dispatcher.h"
 #include "../calico/agents/image_resizer.h"
 #include "../calico/agents/face_detector.h"
 
@@ -54,4 +55,31 @@ TEST(performance_tests, face_detector_maximum_throughput)
 	}
 
 	std::cout << std::format("face_detector maximum throughput measured on {} frames = {:.2f} fps\n", messages_count, throughput.get());
+}
+
+TEST(performance_tests, image_resizer_service_time)
+{
+	constexpr unsigned messages_count = 1000;
+
+	so_5::wrapped_env_t sobjectizer;
+	auto& env = sobjectizer.environment();
+
+	auto input_channel = env.create_mbox();
+	auto output_channel = env.create_mbox();
+	const auto measure_output = create_mchain(env);
+
+	const auto measuring_dispatcher = service_time_estimator_dispatcher::make(env, measure_output->as_mbox(), messages_count);
+	env.introduce_coop(measuring_dispatcher, [&](so_5::coop_t& coop) {
+		coop.make_agent<calico::agents::image_resizer>(input_channel, output_channel, 4);
+	});
+
+	const auto test_frame = cv::imread("test_data/replay/1.jpg");
+	for (auto i = 0u; i < messages_count; ++i)
+	{
+		so_5::send<cv::Mat>(input_channel, test_frame);
+	}
+
+	receive(from(measure_output).handle_n(1), [](double service_time_avg) {
+		std::cout << std::format("image_resizer average service time calculated on {} frames is: {}s\n", messages_count, service_time_avg);
+	});
 }
