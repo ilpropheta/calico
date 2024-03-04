6fc40f9d4227b181a31752b644e691460a775229
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2024-03-04 18:00:54 +0100

    added thread pool skynet

--- calico_tests/benchmarks.cpp
+++ calico_tests/benchmarks.cpp
@@ -89,3 +89,110 @@ TEST(benchmarks, skynet_with_deregistration)
 
 	std::cout << std::chrono::duration<double>(std::chrono::steady_clock::now() - tic) << "\n";
 }
+
+// Same as skynet but with custom dispatcher binder support
+class skynet_tp final : public so_5::agent_t
+{
+public:
+	skynet_tp(so_5::agent_context_t ctx, so_5::disp_binder_shptr_t disp, so_5::mbox_t parent, size_t num, size_t size)
+		: agent_t(std::move(ctx)), m_disp(std::move(disp)), m_parent(std::move(parent)), m_num(num), m_size(size)
+	{}
+
+	void so_evt_start() override
+	{
+		if (1u == m_size)
+		{
+			so_5::send< size_t >(m_parent, m_num);
+		}
+		else
+		{
+			so_subscribe_self().event([this](size_t v) {
+				m_sum += v;
+				if (++m_received == divider)
+				{
+					so_5::send< size_t >(m_parent, m_sum);
+				}
+			});
+
+			so_environment().introduce_coop(
+				m_disp,
+				[&](so_5::coop_t& coop) {
+					const auto sub_size = m_size / divider;
+					for (unsigned int i = 0; i != divider; ++i)
+					{
+						coop.make_agent<skynet_tp>(m_disp, so_direct_mbox(), m_num + i * sub_size, sub_size);
+					}
+			});
+		}
+	}
+
+private:
+	static inline size_t divider = 10;
+
+	const so_5::disp_binder_shptr_t m_disp;
+	const so_5::mbox_t m_parent;
+	const size_t m_num;
+	const size_t m_size;
+	size_t m_sum = 0;
+	size_t m_received = 0;
+};
+
+std::ostream& operator<<(std::ostream& os, so_5::disp::thread_pool::fifo_t fifo)
+{
+	return os << (fifo == so_5::disp::thread_pool::fifo_t::cooperation ? "cooperation" : "individual");
+}
+
+struct skynet_bench_params
+{
+	size_t thread_pool_size;
+	so_5::disp::thread_pool::fifo_t fifo_strategy;
+};
+
+void do_skynet_thread_pool_benchmark(const skynet_bench_params& params)
+{
+	std::chrono::steady_clock::time_point tic;
+	{
+		const so_5::wrapped_env_t sobjectizer;
+		const auto output = create_mchain(sobjectizer.environment());
+
+		tic = std::chrono::steady_clock::now();
+		const auto binder = so_5::disp::thread_pool::make_dispatcher(sobjectizer.environment(), params.thread_pool_size).binder(so_5::disp::thread_pool::bind_params_t{}.fifo(params.fifo_strategy));
+		sobjectizer.environment().introduce_coop([&](so_5::coop_t& coop) {
+			coop.make_agent<skynet_tp>(binder, output->as_mbox(), 0u, 1000000u);
+		});
+
+		size_t result = 0u;
+		receive(from(output).handle_n(1), [&result](size_t v) {
+			result = v;
+		});
+		EXPECT_THAT(result, testing::Eq(499999500000));
+	}
+	std::cout << "skynet thread pool (size=" << params.thread_pool_size << " fifo=" << params.fifo_strategy << ") " << std::chrono::duration<double>(std::chrono::steady_clock::now() - tic) << "\n";
+}
+
+class skynet_thread_pool_benchmarks : public testing::TestWithParam<skynet_bench_params>
+{
+};
+
+TEST_P(skynet_thread_pool_benchmarks, skynet_thread_pool)
+{
+	do_skynet_thread_pool_benchmark(GetParam());
+}
+
+INSTANTIATE_TEST_SUITE_P(cooperation_fifo, skynet_thread_pool_benchmarks,
+	testing::Values(
+		skynet_bench_params{ .thread_pool_size=1, .fifo_strategy=so_5::disp::thread_pool::fifo_t::cooperation },
+		skynet_bench_params{ .thread_pool_size=2, .fifo_strategy=so_5::disp::thread_pool::fifo_t::cooperation },
+		skynet_bench_params{ .thread_pool_size=3, .fifo_strategy=so_5::disp::thread_pool::fifo_t::cooperation },
+		skynet_bench_params{ .thread_pool_size=4, .fifo_strategy=so_5::disp::thread_pool::fifo_t::cooperation },
+		skynet_bench_params{ .thread_pool_size=8, .fifo_strategy=so_5::disp::thread_pool::fifo_t::cooperation }
+));
+
+INSTANTIATE_TEST_SUITE_P(individual_fifo, skynet_thread_pool_benchmarks,
+	testing::Values(
+		skynet_bench_params{ .thread_pool_size = 1, .fifo_strategy = so_5::disp::thread_pool::fifo_t::individual },
+		skynet_bench_params{ .thread_pool_size = 2, .fifo_strategy = so_5::disp::thread_pool::fifo_t::individual },
+		skynet_bench_params{ .thread_pool_size = 3, .fifo_strategy = so_5::disp::thread_pool::fifo_t::individual },
+		skynet_bench_params{ .thread_pool_size = 4, .fifo_strategy = so_5::disp::thread_pool::fifo_t::individual },
+		skynet_bench_params{ .thread_pool_size = 8, .fifo_strategy = so_5::disp::thread_pool::fifo_t::individual }
+));
