5eb9c1c413776edf7742cd4518f3d06fc2fe41c4
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2024-03-12 10:43:23 +0100

    added slip last step

--- calico/agents/routing_slip.h
+++ calico/agents/routing_slip.h
@@ -36,4 +36,25 @@ namespace calico::agents::routing_slip
 		const auto& next_step = msg->next();
 		so_5::send(next_step, std::move(msg));
 	}
+
+	// a generic routing slip "last step" that sends the last message to a specified destination
+	template<typename T>
+	class slip_last_step : public so_5::agent_t
+	{
+	public:
+		slip_last_step(so_5::agent_context_t ctx, so_5::mbox_t destination)
+			: agent_t(ctx), m_destination(std::move(destination))
+		{
+
+		}
+
+		void so_define_agent() override
+		{
+			so_subscribe_self().event([this](so_5::mutable_mhood_t<route_slip_message<T>> msg) {
+				so_5::send<T>(m_destination, std::move(msg->payload));
+			});
+		}
+	private:
+		so_5::mbox_t m_destination;
+	};
 }
