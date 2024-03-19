3d19f98044850c55ff0de3603ac7abc2ca9cf05a
Marco Arena <5806546+ilpropheta@users.noreply.github.com>
2024-03-19 08:58:49 +0100

    added guard_remover

--- calico/agents/image_cache.cpp
+++ calico/agents/image_cache.cpp
@@ -12,9 +12,8 @@ void calico::agents::image_cache::cache_stop_guard::stop() noexcept
 }
 
 calico::agents::image_cache::image_cache(so_5::agent_context_t ctx, so_5::mbox_t input, so_5::mbox_t output, unsigned size)
-	: agent_t(ctx), m_input(std::move(input)), m_output(std::move(output)), m_session_size(size), m_accumulated(0), m_cache(size), m_shutdown_guard(std::make_shared<cache_stop_guard>(so_direct_mbox()))
+	: agent_t(ctx), m_input(std::move(input)), m_output(std::move(output)), m_session_size(size), m_accumulated(0), m_cache(size), m_guard_remover(so_environment(), std::make_shared<cache_stop_guard>(so_direct_mbox()))
 {
-	so_environment().setup_stop_guard(m_shutdown_guard);
 }
 
 void calico::agents::image_cache::so_define_agent()
@@ -32,7 +31,7 @@ void calico::agents::image_cache::so_define_agent()
 
 	so_subscribe_self().event([this](so_5::mhood_t<shutdown_requested>) {
 		flush();
-		so_environment().remove_stop_guard(m_shutdown_guard);
+		m_guard_remover.remove();
 	});
 }
 
--- calico/agents/image_cache.h
+++ calico/agents/image_cache.h
@@ -5,6 +5,37 @@
 
 namespace calico::agents
 {
+	namespace utils
+	{
+		// Manages the given stop_guard in a RAII fashion
+		struct guard_remover
+		{
+			guard_remover(so_5::environment_t& env, so_5::stop_guard_shptr_t guard)
+				: m_env(env), m_guard(std::move(guard))
+			{
+				m_env.setup_stop_guard(m_guard);
+			}
+
+			guard_remover(const guard_remover&) = delete;
+			guard_remover(guard_remover&&) = delete;
+			guard_remover& operator=(const guard_remover&) = delete;
+			guard_remover& operator=(guard_remover&&) = delete;
+
+			~guard_remover()
+			{
+				remove();
+			}
+
+			void remove() const
+			{
+				m_env.remove_stop_guard(m_guard);
+			}
+
+			so_5::environment_t& m_env;
+			so_5::stop_guard_shptr_t m_guard;
+		};
+	}
+
 	// Caches exactly 'size' images and then sends them to the output channel, one by one.
 	// At the shutdown, any remaining data will be delivered to the output channel in any case.
 	class image_cache final : public so_5::agent_t
@@ -30,6 +61,6 @@ namespace calico::agents
 		unsigned m_session_size;
 		unsigned m_accumulated;
 		std::vector<so_5::message_holder_t<cv::Mat>> m_cache;
-		so_5::stop_guard_shptr_t m_shutdown_guard;
+		utils::guard_remover m_guard_remover;
 	};
 }
